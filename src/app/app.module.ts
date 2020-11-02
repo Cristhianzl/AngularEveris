5feaa298b3baf0d8f605fbee7c360f1f820eb2a1
cristhianzl <cristhian.lousa@gmail.com>
2020-11-02 22:42:22 +0100

    Conclusao Aula Intro Angular8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { Component, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
-
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
-
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { Erro404Component } from './404/erro-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
+import { Erro404Component } from './core/component/404/erro-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
-    NavBarComponent,
-    Erro404Component,
-    CourseInfoComponent
+    Erro404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    CoreModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
       {
-        path: '', redirectTo: 'courses', pathMatch: 'full' //rota padrao quando ta na raiz da app
-      },
-      {
-        path: 'course/info/:id', component: CourseInfoComponent
-      },
-      {
-        path: 'courses', component: CourseListComponent
+        path: '', redirectTo: 'courses', pathMatch: 'full'
       },
       {
         path: '**', component: Erro404Component
       }
       ])
-
   ],
   providers: [],
   bootstrap: [AppComponent]
